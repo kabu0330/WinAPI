a11d4b2b1f72e97987598340cf341fb83cd5cd85
성민 류 <baekhun0330@naver.com>
2024-10-22 18:09:29 +0900

    윈도우 업데이트 : lib 인식 문제 해결

--- WinAPI/App/main.cpp
+++ WinAPI/App/main.cpp
@@ -1,9 +1,19 @@
 #include <Windows.h>
+#include <EnginePlatform/EngineWindow.h>
+
+// 더 이상 참조를 사용하지 않고 pragma comment로 라이브러리를 실행파일에 포함
+#pragma comment(lib, "EnginePlatform.lib")
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
-	return 0;
+	UEngineWindow::EngineWindowInit(hInstance);
+	
+	UEngineWindow NewWindow;
+	NewWindow.Open();
+
+	return UEngineWindow::WindowMessageLoop();
 }
---
+++ WinAPI/EnginePlatform/EngineWindow.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <Windows.h>
+
+class UEngineWindow
+{
+public:
+	static void EngineWindowInit(HINSTANCE _Instance);
+
+	static int WindowMessageLoop();
+
+	void Open();
+
+	// constrcuter destructer
+	UEngineWindow();
+	~UEngineWindow();
+
+	// delete Function
+	UEngineWindow(const UEngineWindow& _Other) = delete;
+	UEngineWindow(UEngineWindow&& _Other) noexcept = delete;
+	UEngineWindow& operator=(const UEngineWindow& _Other) = delete;
+	UEngineWindow& operator=(UEngineWindow&& _Other) noexcept = delete;
+
+protected:
+
+private:
+	static HINSTANCE hInstance;
+
+	HWND WindowHandle = nullptr;
+};
+
